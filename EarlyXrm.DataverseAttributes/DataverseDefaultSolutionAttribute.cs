bbaa5ecca13ce29ce2abf8be5110a042df88e9b2
Andrew Berman <andrew.berman@dewr.gov.au>
2023-10-15 22:39:00 +1100

    Add default solution attribute

---
+++ EarlyXrm.DataverseAttributes/DataverseDefaultSolutionAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace EarlyXrm.DataverseAttributes;
+
+using System;
+
+[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false)]
+public class DataverseDefaultSolutionAttribute : Attribute
+{
+    public string Solution { get; set; }
+    public string Prefix { get; set; }
+}
